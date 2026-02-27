da2b6f173241f9acd3cd0539db58fc6da7498cc9
BarefootJoey👣 <barefootjoeytv@gmail.com>
2026-02-27 00:26:56 -0500

    Update check.sh to use python3 and improve commands

--- scripts/check.sh
+++ scripts/check.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
-set -x
 
-python -m compileall contrib_writer >/dev/null
-python contrib_writer/contrib_writer.py --preview --preview-weeks 8 --fit-weeks 52 --text "TEST" --spacing 1 >/dev/null
+python3 -m pip install --upgrade pip >/dev/null
+python3 -m pip install structlog pydantic >/dev/null
 
-python -m pip install --upgrade pip >/dev/null
-python -m pip install structlog pydantic >/dev/null
-python contrib_writer/contrib_writer.py --commit-file /tmp/contrib_writer_check_log.txt --mutation-token check >/dev/null || true
+python3 -m compileall contrib_writer >/dev/null
+python3 contrib_writer/contrib_writer.py --preview --preview-weeks 8 --fit-weeks 52 --text "TEST" --spacing 1 >/dev/null
+
+python3 contrib_writer/contrib_writer.py --commit-file /tmp/contrib_writer_check_log.txt --mutation-token check >/dev/null || true
 
 echo "OK"
